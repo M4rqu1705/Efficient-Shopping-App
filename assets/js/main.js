beb86ec7640eaa358430d655838c737a56c3b929
M4rqu1705 <m4rc05.dev@gmail.com>
2020-05-02 13:28:55 -0400

    Some progresss integrating lottiefiles into landing page. There are a few details I must take care of, but it is showing promise

---
+++ assets/js/main.js
@@ -0,0 +1,104 @@
+/*
+	Projection by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+
+(function($) {
+
+	// Breakpoints.
+		skel.breakpoints({
+			xlarge:	'(max-width: 1680px)',
+			large:	'(max-width: 1280px)',
+			medium:	'(max-width: 980px)',
+			small:	'(max-width: 736px)',
+			xsmall:	'(max-width: 480px)'
+		});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+	// Off-Canvas Navigation.
+
+		// Navigation Panel.
+			$(
+				'<div id="navPanel">' +
+					$('#nav').html() +
+					'<a href="#navPanel" class="close"></a>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left'
+				});
+
+		// Fix: Remove transitions on WP<10 (poor/buggy performance).
+			if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+				$('#navPanel')
+					.css('transition', 'none');
+
+	});
+
+})(jQuery);
+
+
+// Additions to the template --- --- --- ---
+var shoppingCartAnimation = bodymovin.loadAnimation({
+    container: document.getElementById('shopping-cart-lottie'),
+    animationData: shoppingCartAnimationData,
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    rendererSettings: {
+        progressiveLoad:false,
+        preserveAspectRatio: 'xMidYMid slice'
+    }
+})
+
+var mapLocationAnimation = bodymovin.loadAnimation({
+    container: document.getElementById('map-location-lottie'),
+    animationData: mapLocationAnimationData,
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    rendererSettings: {
+        progressiveLoad:false,
+        preserveAspectRatio: 'xMidYMid slice'
+    }
+})
+
+var shoppingListAnimation = bodymovin.loadAnimation({
+    container: document.getElementById('shopping-list-lottie'),
+    animationData: shoppingListAnimationData,
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    rendererSettings: {
+        progressiveLoad:false,
+        preserveAspectRatio: 'xMidYMid slice'
+    }
+})
